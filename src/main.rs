ff404838117daf9302091ee7c882e2e8a963b0a9
Matthias Krüger <matthias.krueger@famsik.de>
2022-09-28 23:21:59 +0200

    move files

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,35 @@ use rayon::prelude::*;
 fn main() -> Result<()> {
     let results = glacier::test_all()?.collect::<Result<Vec<TestResult>, _>>()?;
 
-    let failed = glacier::test_all()?
-        .filter(|res| {
-            if let Ok(test) = res {
-                eprint!("{}", test.outcome_token());
+    let ices = results
+        .iter()
+        .filter(|res| matches!(res.outcome(), Outcome::ICEd));
+    // consider everything that does not crash "fixed"
+    let fixed = results
+        .iter()
+        .filter(|res| !matches!(res.outcome(), Outcome::ICEd));
 
-                test.outcome() != Outcome::ICEd
-            } else {
-                true
-            }
-        })
-        .collect::<Result<Vec<TestResult>, _>>()?;
+    // move files to their right place:
+    // ices to ices
+    ices.map(|res| res.path())
+        // only move ices inside "fixed" folder
+        .filter(|p| p.iter().next().map(|p| p.to_str()).flatten() == Some("fixed"))
+        .for_each(|p| {
+            let from = p;
+            let to = p.to_str().unwrap().replace("fixed", "ices");
+            std::fs::rename(from, to).unwrap();
+        });
 
-    let mut paths = Vec::new();
-    for result in &failed {
-        eprintln!("\n{}", result);
-        paths.push(result.path());
-    }
-
-    match failed.len() {
-        0 => eprintln!("\nFinished: No fixed ICEs"),
-        1 => bail!("1 ICE ({paths:?}) is now fixed!"),
-        len => bail!("{len} ICEs ({paths:?}) are now fixed!"),
-    }
+    // fixed to fixed
+    fixed
+        .map(|res| res.path())
+        // only move fixed files inside "ices" folder
+        .filter(|p| p.iter().next().map(|p| p.to_str()).flatten() == Some("ices"))
+        .for_each(|p| {
+            let from = p;
+            let to = p.to_str().unwrap().replace("ices", "fixed");
+            std::fs::rename(from, to).unwrap();
+        });
 
     Ok(())
 }
