ffef8229d92ee33d08ac08440a20f82b55603902
Matthias Krüger <matthias.krueger@famsik.de>
2022-09-29 21:10:23 +0200

    add more ices

---
+++ unsorted/102252.rs
@@ -0,0 +1,10 @@
+#![feature(min_specialization, rustc_attrs)]
+
+#[rustc_specialization_trait]
+pub trait Trait {}
+
+struct Struct
+where
+    Self: Iterator<Item = <Self as Iterator>::Item>, {}
+
+impl Trait for Struct {}
