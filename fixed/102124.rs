e2fbb3a23cee9cff53fe10cab29f739149ed2c56
Matthias Krüger <matthias.krueger@famsik.de>
2022-10-01 16:03:10 +0200

    fixed: sh -> rs

---
+++ fixed/102124.rs
@@ -0,0 +1,16 @@
+
+const L: usize = 4;
+
+pub trait Print<const N: usize> {
+    fn print(&self) -> usize {
+        N
+    }
+}
+
+pub struct Printer;
+impl Print<L> for Printer {}
+
+fn main() {
+    let p = Printer;
+    assert_eq!(p.print(), 4);
+}
