89184c3f6b868e7aa798030d0d9a1dc3fbf259cc
Matthias Krüger <matthias.krueger@famsik.de>
2022-11-08 00:00:27 +0100

    add another file

---
+++ unsorted/104085.rs
@@ -0,0 +1,8 @@
+// debug-assertion with -Cinstrument-coverage
+fn main() {
+    use std::mem::{transmute, swap};
+    let a = 1;
+    let b = 2;
+    unsafe {swap::<&mut _>(transmute(&a), transmute(&b))};
+    //~^ ERROR type annotations needed
+}
