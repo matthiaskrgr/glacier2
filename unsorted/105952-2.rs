d3df10c3119eaf08fb75cd9a5f05178e3aa566a1
Matthias Krüger <matthias.krueger@famsik.de>
2022-12-20 16:39:13 +0100

    add 2 ices

---
+++ unsorted/105952-2.rs
@@ -0,0 +1,9 @@
+#![feature(associated_const_equality)]
+pub enum ParseMode {
+    Raw,
+}
+pub trait Parse {
+    const PARSE_MODE: ParseMode;
+}
+pub trait RenderRaw {}
+impl<T: Parse<PARSE_MODE = { ParseMode::Raw }>> RenderRaw  for T {}
