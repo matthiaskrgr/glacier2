08de9bf04f43dfde06df1610e0755b3a0a429c64
Matthias Krüger <matthias.krueger@famsik.de>
2023-04-19 18:07:05 +0200

    add

---
+++ unsorted/110453-2.rs
@@ -0,0 +1,10 @@
+pub struct B;
+pub fn a() -> B { B }
+
+mod handlers {
+    pub struct C(B);
+    pub fn c() -> impl Fn() -> C {
+        let a1 = ();
+        || C((crate::a(), a1).into())
+    }
+}
---
+++ unsorted/110464.rs
@@ -0,0 +1,3 @@
+impl<'a, 'tcx> std::cell::Ref<'a, 'tcx> {}
+
+pub fn main() {}
