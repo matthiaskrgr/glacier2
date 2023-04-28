8d8783ba8252d710c794fd6a21d29717aee745b5
Matthias Krüger <matthias.krueger@famsik.de>
2023-04-29 00:24:11 +0200

    more pile

---
+++ pile/8F9D89835FBF3BB16246B2515DB777E95C483C7864BA3ADCF79625FC90371D47.rs
@@ -0,0 +1,9 @@
+fn a() -> _ {
+    //~^ ERROR the placeholder `_` is not allowed within types on item signatures for return types
+    &a
+}
+
+fn a() -> _ {
+    //~^ ERROR the placeholder `_` is not allowed within types on item signatures for return types
+    &a
+}
