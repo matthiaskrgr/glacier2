69c7471dda545b46cd2473e77b59968eba1cce6e
Matthias Krüger <matthias.krueger@famsik.de>
2023-07-30 10:51:57 +0200

    pile++

---
+++ pile/6E0178F13DB080E8C82295AD006DB48D26241E622A127DFB9A910949EA8F6565.rs
@@ -0,0 +1,15 @@
+// Regression test for issue #89938.
+// compile-flags: --crate-type=lib
+// known-bug: #103507
+// failure-status: 101
+// Regression test for issue #89938.
+// normalize-stderr-test "thread 'rustc' panicked.*\n" -> ""
+// normalize-stderr-test "(error: internal compiler error: [^:]+):\d+:\d+: " -> "$1:LL:CC: "
+// rustc-env:RUST_BACKTRACE=0
+
+#![feature(const_precise_live_drops)]
+
+pub const fn f() {
+    let _: Option<String> = None;
+    let _: &'static Option<String> = &None;
+}
---
+++ pile/808B21B4D18549992854E80253E9D87C1E0C7659A5CA40567496F0C3D2771EDC.rs
@@ -0,0 +1,12 @@
+// known-bug: unknown
+// known-bug: unknown
+// ignore-test
+
+#![feature(generic_const_items)]
+#![allow(incomplete_features)]
+
+const RECUR<T>: () = RECUR::<(T,)>;
+
+fn main() {
+    let _ = RECUR::<()>;
+}
