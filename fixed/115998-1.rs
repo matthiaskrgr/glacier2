9426e47124054f5ff12e26d4dc938fea3521471d
Matthias Krüger <matthias.krueger@famsik.de>
2023-09-20 21:36:03 +0200

    upd

--- fixed/115998-1.rs
+++ fixed/115998-1.rs
@@ -1,2 +1,4 @@
 pub struct Data([u8]);
 const _: &'static Data = unsafe { &*(&[] as *const Data) };
+
+pub fn main() {}
--- fixed/115998-2.rs
+++ fixed/115998-2.rs
@@ -1,2 +1,4 @@
 pub struct Data([u8]);
 const _: *const Data = &[] as *const Data;
+
+pub fn main() {}
