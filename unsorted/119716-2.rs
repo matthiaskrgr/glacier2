800ffe067bb221353574ed01f9424f3f35992cdd
Matthias Krüger <matthias.krueger@famsik.de>
2024-01-09 21:26:30 +0100

    ice++

---
+++ unsorted/119716-2.rs
@@ -0,0 +1,3 @@
+#![feature(non_lifetime_binders)]
+trait Trait<T> {}
+fn f<T>() -> impl for<T> Trait<impl Trait<T>> {}
---
+++ unsorted/119716.rs
@@ -0,0 +1,3 @@
+#![feature(non_lifetime_binders)]
+trait v0<v1> {}
+fn kind  :(v3main impl for<v4> v0<'_, v2 = impl v0<v4> + '_>) {}
