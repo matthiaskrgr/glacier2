03374bc27a7ee69fcd64d0f9756fe518dbc09336
Matthias Krüger <matthias.krueger@famsik.de>
2024-02-13 06:38:11 +0100

    ice++

---
+++ unsorted/112623-3.rs
@@ -0,0 +1,21 @@
+trait Value {
+    fn value() -> u32;
+}
+
+const fn get_value<T: ~const Value>() -> u32 {
+    T::value()
+}
+
+struct FortyTwo;
+
+impl const Value for FortyTwo {
+    fn value() -> i64 {
+        42
+    }
+}
+
+const FORTY_TWO: u32 = get_value::<FortyTwo>();
+
+fn main() {
+    assert_eq!(FORTY_TWO, 42);
+}
